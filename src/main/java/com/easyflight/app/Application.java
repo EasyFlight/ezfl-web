bb3cff3c5ee3e3991d553d943aea79d62ef8cfe5
Victor Ikoro <victor.ikoro@interswitchng.com>
2017-07-11 20:24:53 +0100

    #Initial Commit

--- src/main/java/com/easyflight/app/Application.java
+++ src/main/java/com/easyflight/app/Application.java
@@ -2,11 +2,13 @@ package com.easyflight.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by Victor.Ikoro on 7/11/2017.
  */
 @SpringBootApplication
+@EnableZuulProxy
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
