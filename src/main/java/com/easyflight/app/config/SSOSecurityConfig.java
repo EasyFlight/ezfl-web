6a2690f9ee0b319ce3043fbcc22a05fd8dfcb785
Victor Ikoro <victor.ikoro@interswitchng.com>
2017-09-13 10:30:05 +0100

    test

--- src/main/java/com/easyflight/app/config/SSOSecurityConfig.java
+++ src/main/java/com/easyflight/app/config/SSOSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
